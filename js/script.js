735e1b44ca73ebe1ce9eef37838d65c0f51c227d
ashotivanyan <ivanyan_06@mail.ru>
2024-07-16 09:55:07 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+console.log(25 + 55);
+console.log(11 - 4);
+console.log(20 / 5);
+console.log(6 * 9);
+console.log(5 % 2);
+console.log(21 + 5 * 9);
+console.log((10 + 6) * 5);
+
+console.log(7 > 4);
+console.log(8 < 2);
+console.log(10 >= 9);
+console.log(8 <= 8);
+console.log(25 == 25);
+console.log(25 === 25);
+console.log(30 != "30");
+console.log(20 !== "20");
+
+console.log(3 >= 7 && 10 === 10);
+console.log(5 >=3 && 2 === 2);
+
+console.log(10 > 30 || 1 === 2);
