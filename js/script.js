4874df789dcc6e9636c28a2fdb82eb2f2808b488
ashotivanyan <ivanyan_06@mail.ru>
2024-07-27 12:50:44 +0300

    On project we added functions

--- js/script.js
+++ js/script.js
@@ -1,24 +1,98 @@
 "use strict";
 
-const countOfFilms = +prompt("How many movies have you watche ever ?");
+let countOfFilms;
+
+init();
+
+function init() {
+  countOfFilms = +prompt(" Քանի՞ ֆիլմ եք վերջերս դիտել");
+
+  while (countOfFilms == "" || countOfFilms == null || isNaN(countOfFilms)) {
+    countOfFilms = +prompt(" Քանի՞ ֆիլմ եք վերջերս դիտել");
+  }
+}
 
 const DB = {
-    count: countOfFilms,
-    movies: {},
-    actors: {},
-    genrer: [],
-    privat: false
+  count: countOfFilms,
+  movies: {},
+  actors: {},
+  genrer: [],
+  status: null,
+  privat: false
 };
 
-const a = prompt("What movie did you watch recently?");
-const b = +prompt("How much do you rate that movie?");
-const c = prompt("What movie did you watch recently?");
-const d = +prompt("How much do you rate that movie?");
-const e = prompt("What movie did you watch recently?");
-const f = +prompt("How much do you rate that movie?");
 
-DB.movies[a] = b;
-DB.movies[c] = d;
-DB.movies[e] = f;
+function rememberAndCreateUserFilms() {
+  for (let i = 0; i < 1; i++) {
+    const filmName = prompt("1) Ո՞ր ֆիլմն եք վերջերս դիտել");
+    const filmRate = +prompt(`1) Ինչքա՞ն եք գնահատում ${filmName} ֆիլմը`);
 
-console.log(DB);
+    if (filmName != null && filmRate != null && filmName != "" && filmRate != "" && filmName.length < 50) {
+      DB.movies[filmName] = filmRate;
+      console.log("Resolve");
+    } else {
+      console.log("Reject");
+      i--;
+    }
+  }
+  for (let i = 0; i < 1; i++) {
+    const filmName = prompt("2) Ո՞ր ֆիլմն եք վերջերս դիտել");
+    const filmRate = +prompt(`2) Ինչքա՞ն եք գնահատում ${filmName} ֆիլմը`);
+
+    if (filmName != null && filmRate != null && filmName != "" && filmRate != "" && filmName.length < 50) {
+      DB.movies[filmName] = filmRate;
+      console.log("Resolve");
+    } else {
+      console.log("Reject");
+      i--;
+    }
+  }
+}
+
+function userFavoriteGenres(){
+  for (let i = 0; i < 1; i++) {
+    const filmGenres = prompt("1) Ձեր սիրելի ֆիլմի ժանրը");
+    const filmGenres2 = +prompt("2) Ձեր սիրելի ֆիլմի ժանրը");
+    
+    
+    if (filmGenres != null && filmGenres2 != null && filmGenres != "" && filmGenres2 != "" && filmGenres.length < 50) {
+      DB.movies[filmGenres] = filmGenres2;
+      console.log("Resolve");
+    } else {
+      console.log("Reject");
+      i--;
+    }
+  }
+}
+  for (let i = 0; i < 1; i++) {
+    const filmTime = prompt("Ո՞ր ժամն եք նախընտրում: ֆիլմ դիտելու համար");
+    
+    if (filmTime != null && filmTime != ""  && filmTime.length < 50) {
+      DB.movies[filmTime] = filmTime ;
+      console.log("Resolve");
+    } else {
+      console.log("Reject");
+      i--;
+    }
+  }
+
+function detectUserPersonalLevel() {
+  if (DB.count < 10) {
+    DB.status = "Դուք դիտել եք բավականին քիչ ֆիլմեր";
+  } else if (DB.count >= 10 && DB.count < 30) {
+    DB.status = "Դուք դասական ֆիլմ դիտող եք";
+  } else if (DB.count >= 30) {
+    DB.status = "Դուք կինոման եք !"
+  } else {
+    console.log("Տեղի է ունեցել խնդիր․");
+  }
+}
+
+function checkDBPrivatStatus(state) {
+  state ? console.log("Sorry but DB is not visible") : console.log(DB);
+}
+
+ rememberAndCreateUserFilms();
+ userFavoriteGenres();
+ detectUserPersonalLevel();
+checkDBPrivatStatus(DB.privat);
